86eee767910ffe29c89edb38748cfc91d1c0a7e1
Pyvincent <wangertx@aliyun.com>
2016-09-22 22:14:56 +0800

    haha

--- app/src/main/java/com/wang/vincent/modebuttontest/MainActivity.java
+++ app/src/main/java/com/wang/vincent/modebuttontest/MainActivity.java
@@ -1,32 +1,66 @@
 package com.wang.vincent.modebuttontest;
 
+import android.content.Context;
 import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.util.DisplayMetrics;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
     static {
         System.loadLibrary("native-lib");
     }
+
     MyBRReceiver myBRReceiver;
+    GetDeviceData getDeviceData = new GetDeviceData();
+
+    private List<Animal> mData = null;
+    private Context mContext;
+    private AnimalAdapter mAdapter = null;
+    private ListView list_animal;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-
+        mContext = MainActivity.this;
+        list_animal = (ListView) findViewById(R.id.list_animal);
+        mData = new LinkedList<Animal>();
+        mData.add(new Animal("系统版本号", Build.DISPLAY, R.mipmap.ic_launcher));
+        mData.add(new Animal("CPU", getDeviceData.getCpuName(), R.mipmap.ic_launcher));
+        mData.add(new Animal("CPU核心数", "" + getDeviceData.getNumCores(), R.mipmap.ic_launcher));
+        mData.add(new Animal("获取rom大小", "" + getDeviceData.getTotalInternalMemorySize(), R.mipmap.ic_launcher));
+        mData.add(new Animal("分辨率", getScreenResolution(), R.mipmap.ic_launcher));
+        mAdapter = new AnimalAdapter((LinkedList<Animal>) mData, mContext);
+        list_animal.setAdapter(mAdapter);
 
         myBRReceiver = new MyBRReceiver();
         IntentFilter itFilter = new IntentFilter();
         itFilter.addAction("android.intent.action.ENG_MODE_SWITCH");
         registerReceiver(myBRReceiver, itFilter);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -35,9 +69,49 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(myBRReceiver);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
+    public String getScreenResolution() {
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        String strOpt = dm.widthPixels + " * " + dm.heightPixels;
+        return strOpt;
+    }
+
+
     public native String stringFromJNI();
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
